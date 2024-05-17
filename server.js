29a9f0cdcdfdf601c4784ff68ea0c169da741664
Ben <phatben234@gmail.com>
2024-05-17 11:50:20 +0700

    update server

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const connectDB = require("./db/connect.js");
+const tasks = require('./routes/router.js');
+const connectDB = require("./database/connect.js");
 
-dotenv.config({path: '.env'})
-const PORT = process.env.PORT || 8080
-
-connectDB();
+dotenv.config({ path: ".env" });
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1/tasks', tasks);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(process.env.MONGO_URL);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+startServer();
