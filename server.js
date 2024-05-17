f7e4dd6875d022698ca35def117f265cbcf8ca2c
Ben <phatben234@gmail.com>
2024-05-17 10:12:47 +0700

    update server

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
-const path = require("path");
 const connectDB = require("./db/connect.js");
 
-const app = express();
-
-dotenv.config({ path: "config.env" });
-const PORT = process.env.PORT || 8080;
-
-require("dotenv").config();
+dotenv.config({path: '.env'})
+const PORT = process.env.PORT || 8080
 
 connectDB();
 
-// parse request to body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
